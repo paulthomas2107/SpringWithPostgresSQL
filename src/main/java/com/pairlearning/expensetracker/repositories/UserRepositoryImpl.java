63f923315c7bc5aeb6f2ac599abcf696b5e46acf
Paul Thomas <paulthomas2107@gmail.com>
2020-10-22 16:24:20 +0100

    Hello again

--- src/main/java/com/pairlearning/expensetracker/repositories/UserRepositoryImpl.java
+++ src/main/java/com/pairlearning/expensetracker/repositories/UserRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.pairlearning.expensetracker.repositories;
 
 import com.pairlearning.expensetracker.domain.User;
 import com.pairlearning.expensetracker.exceptions.EtAuthException;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -30,6 +31,9 @@ public class UserRepositoryImpl implements UserRepository{
 
     @Override
     public Integer create(String firstName, String lastName, String email, String password) throws EtAuthException {
+
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(10));
+
         try {
 
             KeyHolder keyHolder = new GeneratedKeyHolder();
@@ -38,7 +42,7 @@ public class UserRepositoryImpl implements UserRepository{
                 ps.setString(1, firstName);
                 ps.setString(2, lastName);
                 ps.setString(3, email);
-                ps.setString(4, password);
+                ps.setString(4, hashedPassword);
                 return ps;
             }, keyHolder);
             return (Integer) keyHolder.getKeys().get("USER_ID");
@@ -52,7 +56,7 @@ public class UserRepositoryImpl implements UserRepository{
         try {
 
             User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL, new Object[]{email}, userRowMapper);
-            if (!password.equals(user.getPassword())) {
+            if (!BCrypt.checkpw(password, user.getPassword())) {
                 throw new EtAuthException("Invalid Email/Password");
             } else {
                 return user;
