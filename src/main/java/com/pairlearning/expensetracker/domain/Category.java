c513f0b27fc7cbd03976e1dec7cd892c27251768
Paul Thomas <paulthomas2107@gmail.com>
2020-10-26 12:33:44 +0000

    Hello again

---
+++ src/main/java/com/pairlearning/expensetracker/domain/Category.java
@@ -0,0 +1,20 @@
+package com.pairlearning.expensetracker.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    private Integer categoryId;
+    private Integer userId;
+    private String title;
+    private String description;
+    private Double totalExpense;
+
+}
+
+
